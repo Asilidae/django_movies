2cb479f955f3e7047025be69d2568b9e12ab208d
Asilidae <50028633+Asilidae@users.noreply.github.com>
2020-04-02 00:00:38 +0300

    настройка административной панели

--- movies/admin.py
+++ movies/admin.py
@@ -1,11 +1,63 @@
 from django.contrib import admin
-from .models import Category, Actor, Genre, Movie, MovieShots, RatingStar, Rating, Reviews
-# Register your models here.
-admin.site.register(Category)
+from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)
+
+class ReviewInline(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email")
+
+@admin.register(Movie)
+class MovieyAdmin(admin.ModelAdmin):
+    """Фильмы"""
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year") #фильтры
+    search_fields = ("title", "category__name")
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    #list_editable = ("draft",)
+    #fields = (("actors", "directors", "genres"), )
+    fieldsets = (
+        (None, {
+            "fields": (("title", "tagline"),)
+        }),
+        (None, {
+            "fields": ("description", "poster")
+        }),
+        (None, {
+            "fields": (("year", "world_premiere", "country"),)
+        }),
+        ("Актеры", {
+            "classes": ("collapse",),
+            "fields": (("actors", "directors", "genres", "category"),)
+        }),
+        (None, {
+            "fields": (("budget", "fees_in_usa", "fess_in_world"),)
+        }),
+        ("Опции", {
+            "fields": (("url", "draft"),)
+        }),
+    )
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    """Отзывы"""
+    list_display = ("name", "email", "parent", "movie", "id")
+    readonly_fields = ("name", "email")
+
+
+
 admin.site.register(Actor)
 admin.site.register(Genre)
-admin.site.register(Movie)
+
 admin.site.register(MovieShots)
 admin.site.register(RatingStar)
 admin.site.register(Rating)
-admin.site.register(Reviews)
+
