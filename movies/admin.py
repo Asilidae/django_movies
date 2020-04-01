96bafdd420d19150329cbf7c623a49fbd667b85d
Asilidae <50028633+Asilidae@users.noreply.github.com>
2020-04-02 01:02:29 +0300

    Добавление изображений в админ. панели

--- movies/admin.py
+++ movies/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 
@@ -8,43 +10,58 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "url")
     list_display_links = ("name",)
 
+
 class ReviewInline(admin.TabularInline):
+    """Отзывы на странице фильма"""
     model = Reviews
     extra = 1
     readonly_fields = ("name", "email")
 
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ("get_image", )
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="110"')
+
+    get_image.short_description = "Изображение"
+
 @admin.register(Movie)
-class MovieyAdmin(admin.ModelAdmin):
+class MovieAdmin(admin.ModelAdmin):
     """Фильмы"""
     list_display = ("title", "category", "url", "draft")
-    list_filter = ("category", "year") #фильтры
+    list_filter = ("category", "year")
     search_fields = ("title", "category__name")
-    inlines = [ReviewInline]
+    inlines = [MovieShotsInline, ReviewInline ]
     save_on_top = True
     save_as = True
-    #list_editable = ("draft",)
-    #fields = (("actors", "directors", "genres"), )
+    list_editable = ("draft",)
+    readonly_fields = ("get_image", )
     fieldsets = (
         (None, {
             "fields": (("title", "tagline"),)
         }),
         (None, {
-            "fields": ("description", "poster")
+            "fields": ("description", ("poster", "get_image"))
         }),
         (None, {
             "fields": (("year", "world_premiere", "country"),)
         }),
-        ("Актеры", {
+        ("Actors", {
             "classes": ("collapse",),
             "fields": (("actors", "directors", "genres", "category"),)
         }),
         (None, {
             "fields": (("budget", "fees_in_usa", "fess_in_world"),)
         }),
-        ("Опции", {
+        ("Options", {
             "fields": (("url", "draft"),)
         }),
     )
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="100" height="110"')
+
+    get_image.short_description = "Постер"
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
@@ -53,11 +70,41 @@ class ReviewAdmin(admin.ModelAdmin):
     readonly_fields = ("name", "email")
 
 
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Жанры"""
+    list_display = ("name", "url")
 
-admin.site.register(Actor)
-admin.site.register(Genre)
 
-admin.site.register(MovieShots)
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    """Актеры"""
+    list_display = ("name", "age", "get_image")
+    readonly_fields = ("get_image", )
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = "Изображение"
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Рейтинг"""
+    list_display = ("star", "movie", "ip")
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    """Кадры из фильма"""
+    list_display = ("title", "movie", "get_image")
+    readonly_fields = ("get_image", )
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="60" height="60"')
+
+    get_image.short_description = "Изображение"
+
+
 admin.site.register(RatingStar)
-admin.site.register(Rating)
 
+admin.site.site_title = "Сайт о фильмах"
+admin.site.site_header = "Сайт о фильмах"
